166de5dbeccdd1d5f94e9e6627f4fce3e75222fa
AtmakuriPradeep-2 <atmakuripradeep0@gmail.com>
2025-02-09 21:40:49 +0530

    Fix: Defined multer upload middleware

--- routes/firmRoutes.js
+++ routes/firmRoutes.js
@@ -3,7 +3,6 @@ const firmController=require('../controllers/firmController');
 const verifyToken=require('../middlewares/verifyTokens');
 const router=  express.Router()
 router.post('/add-firm',verifyToken,firmController.addFirm);
-router.post("/add", upload.single("image"), addFirm);
 
 router.get('/uploads/:imageName',(req,res)=>{
     const imageName=req.params.imageName;
